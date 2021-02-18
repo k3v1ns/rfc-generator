b85410393b35d7fc16f9ada65b347c79a0cf5271
k3v1ns <k3v1ng45c4@gmail.com>
2021-02-17 23:42:35 -0600

    se cumple con la regla 4 y 5

--- js/01-if-else.js
+++ js/01-if-else.js
@@ -1,31 +1,31 @@
 
-let tipoChacra = "agua";
+// let tipoChacra = "agua";
 
-if(tipoChacra == "fuego"){
-    console.log ("tu chacra es de fuego")
-}else if (tipoChacra == "aire"){
-    console.log ("tu chacra es de aire")
-}else if(tipoChacra == "agua"){
-    console.log ("te chingo con trueno");
-}
-else {
-    console.log ("entonces tu chacra es tierra")
-}
+// if(tipoChacra == "fuego"){
+//     console.log ("tu chacra es de fuego")
+// }else if (tipoChacra == "aire"){
+//     console.log ("tu chacra es de aire")
+// }else if(tipoChacra == "agua"){
+//     console.log ("te chingo con trueno");
+// }
+// else {
+//     console.log ("entonces tu chacra es tierra")
+// }
 
 
 
-caraMoneda = ""
+// caraMoneda = ""
 
-if(caraMoneda == "sol")
-{
-    console.log ("Sol")
+// if(caraMoneda == "sol")
+// {
+//     console.log ("Sol")
 
-}else if(caraMoneda == "Aguila"){
-    console.log ("Aguila")
-}
-else{
-    console.log ("Parada")
-}
+// }else if(caraMoneda == "Aguila"){
+//     console.log ("Aguila")
+// }
+// else{
+//     console.log ("Parada")
+// }
 
 
 // if(caraMoneda == "sol")
--- js/scriptsKevin.js
+++ js/scriptsKevin.js
@@ -6,45 +6,103 @@ console.log("Mi primer script");
         1.- Primer Vocal del apellido Paterno   OK 
         2.- Primera letra del apellido Materno  OK 
         3.- Primera letra el primer Nombre
-            3.1.- Debera conciderar las excepciones de JOSE y de MARIA.     IN PROGRESS
-        4.- Debera Colocar la fecha de nacimiento en formato 'aaMMdd'.      TO DO
-        5.- DEBERA COLOCAR UNA X si el acronimo formula las siguiente palabras 
+            3.1.- Debera conciderar las excepciones de JOSE y de MARIA.     OK
+        4.- Debera Colocar la fecha de nacimiento en formato 'aaMMdd'.      OK
+        5.- DEBERA COLOCAR UNA X si el acronimo formula las siguiente palabras FY  
             - PENE
             - CULO 
             - CACA
+            - PITO
+            - PUTO
+            - PUTA
+
  */
 
-let primerNombre = "MAria";
-let segundoNombre = "Kevin";
-let apellidoPaterno = "Gasca";
-let apellidoMaterno = "Pérez";
+let primerNombre = "jose";
+let segundoNombre = "ana";
+let apellidoPaterno = "puerta";
+let apellidoMaterno = "torres";
 let fechaNacimiento = "01/11/2002"; 
-const rfcEsperado = "GAPK021101"
 
+///
+/// esta variable sirve para almacenar el valor del RFC final 
+///
+let rfcCalculado;
 
+///
+///  esta variable sirve como auxiliar para la regal 3.1 
+/// 
 let nombreFinal;
 
+///
+/// esta variable usa las primeras cuatro letras del RFC
+///
+let inicioRfc;
+
+///
+///  esta condicion sirve para saber si el primer nombre es jose 
+///  
 if (primerNombre.toUpperCase() == "JOSE") {
     
     nombreFinal = segundoNombre; 
     
-} else if(primerNombre.toUpperCase() == "MARIA") {
+} 
+///   
+///  si no es jose pero si es maria 
+/// 
+else if(primerNombre.toUpperCase() == "MARIA") {
   
   nombreFinal = segundoNombre;
-} else {
+} 
+///
+///   si no es jose ni maria si sigue usando el primer nombre 
+///
+else {
     nombreFinal = primerNombre;
+}
+
+inicioRfc = apellidoPaterno.substr(0,2)
+            + apellidoMaterno.substr(0,1) 
+            + nombreFinal.substr(0,1);
+
+if(inicioRfc.toUpperCase() ==  "PENE"){
+
+    inicioRfc = "PENX";
+}
+else if(inicioRfc.toUpperCase() == "CULO"){
+
+    inicioRfc = "CULX";
+}
+
+else if(inicioRfc.toUpperCase() == "CACA"){
+
+   inicioRfc = "CACX";
+}
+
+else if(inicioRfc.toUpperCase() == "PITO"){
+
+    inicioRfc = "PITX";
+}
+
+else if(inicioRfc.toUpperCase() == "PUTO"){
+
+    inicioRfc = "PUTX";
+
+}
+
+else if(inicioRfc.toUpperCase() == "PUTA"){
+
+    inicioRfc = "PUTX";
 }
 
 ///
 ///  Se extrajeron y concatenaron caracteres de los datos para hacer un RFC   
 ///
-let rfcCalculado = apellidoPaterno.substr(0,2)
-                    + apellidoMaterno.substr(0,1) 
-                    + nombreFinal.substr(0,1)
+rfcCalculado = inicioRfc
                     + fechaNacimiento.substr(8,2)
                     + fechaNacimiento.substr(3,2)
                     + fechaNacimiento.substr(0,2);
-
+                    
 ///
 ///   se utiliza la función .toUpperCase para convertir todo en mayusculas 
 ///   
@@ -55,8 +113,6 @@ rfcCalculado = rfcCalculado.toUpperCase();
 ///   en el segundo console.log imprime el rfcEsperado 
 ///   En el tercer console.log compara los resultados y te dice si esta bien 
 console.log("RFC calculado: " + rfcCalculado);
-console.log("RFC user: " + rfcEsperado);
-console.log(rfcCalculado == rfcEsperado);
 
 
 
@@ -74,17 +130,7 @@ console.log(rfcCalculado == rfcEsperado);
 
 
 
-let concatenarUno = primerNombre + " " + segundoNombre;
-let concatenarDos =  `Mi RFC es: ${primerNombre} ${segundoNombre} ${apellidoPaterno} ${apellidoMaterno}`;
-let concatenarTres  =  concatenarUno + concatenarDos;
 
 
 
-// console.log("(1,2): "    + apellidoPaterno.substr(0,2));  
-// console.log("(1,2): "    + apellidoMaterno.substr(0,1));  
-// console.log("(1,2): "    + primerNombre.substr(0,1));   
 
-// console.log("(1,2): "    + fechaNacimiento.substr(8,2));   
-  
-// console.log("(5,7): "    + fechaNacimiento.substr(3,2));   
-// console.log("(1,4): "    + fechaNacimiento.substr(0,2));   
