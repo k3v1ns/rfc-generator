a06b8bc8744fd22b5545dc93c75ea8b29d6d2b1a
Roberto <Garcia>
2021-02-18 22:01:03 -0600

    nuevo script a realizar.

---
+++ js/RfcGenerator.js
@@ -0,0 +1,125 @@
+/*
+    Este escript debera calcular el RFC de una persona dada su informacion personal. 
+    Rules: 
+        1.- Primer Vocal del apellido Paterno   OK 
+        2.- Primera letra del apellido Materno  OK 
+        3.- Primera letra el primer Nombre
+            3.1.- Debera conciderar las excepciones de JOSE y de MARIA.     OK
+        4.- Debera Colocar la fecha de nacimiento en formato 'aaMMdd'.      OK
+        5.- DEBERA COLOCAR UNA X si el acronimo formula las siguiente palabras FY  
+            - PENE
+            - CULO 
+            - CACA
+            - PITO
+            - PUTO
+            - PUTA
+
+ */
+
+let rfc =  generateRFC( "jose","ROBERTO","garcia","sanchez","19/07/1990");
+
+
+console.log("ESTO ES UNA PRUEBA DE RESULTADOS: " + rfc)
+
+
+function generateRFC (
+    primerNombre,
+    segundoNombre,
+    apellidoPaterno,
+    apellidoMaterno,
+    fechaNacimiento
+)
+{
+        
+    ///
+    /// esta variable sirve para almacenar el valor del RFC final 
+    ///
+    let rfcCalculado;
+
+    ///
+    ///  esta variable sirve como auxiliar para la regal 3.1 
+    /// 
+    let nombreFinal;
+
+    ///
+    /// esta variable usa las primeras cuatro letras del RFC
+    ///
+    let inicioRfc;
+
+    ///
+    ///  esta condicion sirve para saber si el primer nombre es jose 
+    ///  
+    if (primerNombre.toUpperCase() == "JOSE") {
+        
+        nombreFinal = segundoNombre; 
+        
+    } 
+    ///   
+    ///  si no es jose pero si es maria 
+    /// 
+    else if(primerNombre.toUpperCase() == "MARIA") {
+    
+    nombreFinal = segundoNombre;
+    } 
+    ///
+    ///   si no es jose ni maria si sigue usando el primer nombre 
+    ///
+    else {
+        nombreFinal = primerNombre;
+    }
+
+    inicioRfc = apellidoPaterno.substr(0,2)
+                + apellidoMaterno.substr(0,1) 
+                + nombreFinal.substr(0,1);
+
+    if(inicioRfc.toUpperCase() ==  "PENE"){
+
+        inicioRfc = "PENX";
+    }
+    else if(inicioRfc.toUpperCase() == "CULO"){
+
+        inicioRfc = "CULX";
+    }
+
+    else if(inicioRfc.toUpperCase() == "CACA"){
+
+    inicioRfc = "CACX";
+    }
+
+    else if(inicioRfc.toUpperCase() == "PITO"){
+
+        inicioRfc = "PITX";
+    }
+
+    else if(inicioRfc.toUpperCase() == "PUTO"){
+
+        inicioRfc = "PUTX";
+
+    }
+
+    else if(inicioRfc.toUpperCase() == "PUTA"){
+
+        inicioRfc = "PUTX";
+    }
+
+    ///
+    ///  Se extrajeron y concatenaron caracteres de los datos para hacer un RFC   
+    ///
+    rfcCalculado = inicioRfc
+                        + fechaNacimiento.substr(8,2)
+                        + fechaNacimiento.substr(3,2)
+                        + fechaNacimiento.substr(0,2);
+                        
+    ///
+    ///   se utiliza la función .toUpperCase para convertir todo en mayusculas 
+    ///   
+    rfcCalculado = rfcCalculado.toUpperCase();
+
+    ///  
+    ///   en el primer console.log imprime el rfc calculado con los datos que tienes
+    ///   en el segundo console.log imprime el rfcEsperado 
+    ///   En el tercer console.log compara los resultados y te dice si esta bien 
+   
+
+    return rfcCalculado;
+}
--- js/scriptsKevin.js
+++ js/scriptsKevin.js
@@ -1,136 +1,14 @@
-console.log("Mi primer script");
+console.log("script cuentas bancarias");
 
 /*
-    Este escript debera calcular el RFC de una persona dada su informacion personal. 
-    Rules: 
-        1.- Primer Vocal del apellido Paterno   OK 
-        2.- Primera letra del apellido Materno  OK 
-        3.- Primera letra el primer Nombre
-            3.1.- Debera conciderar las excepciones de JOSE y de MARIA.     OK
-        4.- Debera Colocar la fecha de nacimiento en formato 'aaMMdd'.      OK
-        5.- DEBERA COLOCAR UNA X si el acronimo formula las siguiente palabras FY  
-            - PENE
-            - CULO 
-            - CACA
-            - PITO
-            - PUTO
-            - PUTA
+    1.- Este script debera saber si el deposito se trata de una:
 
+        * TARJETA (CREDITO / DEBITO)
+        * CLABE INTERBANCARIA
+        * CUENTA  
+    2.- Solo debe tomar en cuenta numeros
+    3.- Si cumple con los 2 puntos anteriores mandar una bandera (TRUE ) en caso contrario (FALSE)
  */
-
-let primerNombre = "jose";
-let segundoNombre = "ana";
-let apellidoPaterno = "puerta";
-let apellidoMaterno = "torres";
-let fechaNacimiento = "01/11/2002"; 
-
-///
-/// esta variable sirve para almacenar el valor del RFC final 
-///
-let rfcCalculado;
-
-///
-///  esta variable sirve como auxiliar para la regal 3.1 
-/// 
-let nombreFinal;
-
-///
-/// esta variable usa las primeras cuatro letras del RFC
-///
-let inicioRfc;
-
-///
-///  esta condicion sirve para saber si el primer nombre es jose 
-///  
-if (primerNombre.toUpperCase() == "JOSE") {
-    
-    nombreFinal = segundoNombre; 
-    
-} 
-///   
-///  si no es jose pero si es maria 
-/// 
-else if(primerNombre.toUpperCase() == "MARIA") {
-  
-  nombreFinal = segundoNombre;
-} 
-///
-///   si no es jose ni maria si sigue usando el primer nombre 
-///
-else {
-    nombreFinal = primerNombre;
-}
-
-inicioRfc = apellidoPaterno.substr(0,2)
-            + apellidoMaterno.substr(0,1) 
-            + nombreFinal.substr(0,1);
-
-if(inicioRfc.toUpperCase() ==  "PENE"){
-
-    inicioRfc = "PENX";
-}
-else if(inicioRfc.toUpperCase() == "CULO"){
-
-    inicioRfc = "CULX";
-}
-
-else if(inicioRfc.toUpperCase() == "CACA"){
-
-   inicioRfc = "CACX";
-}
-
-else if(inicioRfc.toUpperCase() == "PITO"){
-
-    inicioRfc = "PITX";
-}
-
-else if(inicioRfc.toUpperCase() == "PUTO"){
-
-    inicioRfc = "PUTX";
-
-}
-
-else if(inicioRfc.toUpperCase() == "PUTA"){
-
-    inicioRfc = "PUTX";
-}
-
-///
-///  Se extrajeron y concatenaron caracteres de los datos para hacer un RFC   
-///
-rfcCalculado = inicioRfc
-                    + fechaNacimiento.substr(8,2)
-                    + fechaNacimiento.substr(3,2)
-                    + fechaNacimiento.substr(0,2);
-                    
-///
-///   se utiliza la función .toUpperCase para convertir todo en mayusculas 
-///   
-rfcCalculado = rfcCalculado.toUpperCase();
-
-///  
-///   en el primer console.log imprime el rfc calculado con los datos que tienes
-///   en el segundo console.log imprime el rfcEsperado 
-///   En el tercer console.log compara los resultados y te dice si esta bien 
-console.log("RFC calculado: " + rfcCalculado);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
